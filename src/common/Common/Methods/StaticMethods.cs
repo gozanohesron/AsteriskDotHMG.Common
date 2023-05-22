d27a49356d1ff797f88ac5a2e5ce7f44e6c87615
Hesron Gozano <hesron.gozano@live.com>
2023-05-22 17:43:47 +0800

    Add password genetator event

--- src/common/Common/Methods/StaticMethods.cs
+++ src/common/Common/Methods/StaticMethods.cs
@@ -24,4 +24,34 @@ public static partial class StaticMethods
 
         return result;
     }
+
+    public static string GeneratePassword(int length = 12, bool includeUppercase = true, bool includeDigits = true, bool includeSpecialChars = true)
+    {
+        string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string numbers = "0123456789";
+        string specialCharacters = "!@#$%^&*()";
+
+    StringBuilder passwordBuilder = new();
+
+        // Create a pool of characters based on the selected options
+        string characters = lowerCase;
+        if (includeUppercase)
+            characters += upperCase;
+        if (includeDigits)
+            characters += numbers;
+        if (includeSpecialChars)
+            characters += specialCharacters;
+
+        Random random = new();
+
+        // Generate random password
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = random.Next(characters.Length);
+            passwordBuilder.Append(characters[randomIndex]);
+        }
+
+        return passwordBuilder.ToString();
+    }
 }
