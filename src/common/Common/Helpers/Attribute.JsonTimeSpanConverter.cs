e43145af17cd3006a937b97f93ba3ed5f098adf2
Hesron Gozano <hesron.gozano@live.com>
2023-11-24 18:40:28 +0800

    Add JsonTimeSpanConverter

---
+++ src/common/Common/Helpers/Attribute.JsonTimeSpanConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace AsteriskDotHMG.Common.Helpers;
+
+public class JsonTimeSpanConverter : STJ.JsonConverter<TimeSpan>
+{
+    public bool WithoutSeconds { get; init; }
+
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var input = reader.GetString();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return default;
+        }
+
+        return TimeSpan.Parse(input);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(
+            WithoutSeconds
+                ? $"{value.Hours:D2}:{value.Minutes:D2}"
+                : value.ToString("c"));
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class JsonTimeSpanConverterAttribute : STJ.JsonConverterAttribute
+{
+    public bool WithoutSeconds { get; init; }
+
+    public override STJ.JsonConverter CreateConverter(Type typeToConvert)
+    {
+        return new JsonTimeSpanConverter
+        {
+            WithoutSeconds = WithoutSeconds
+        };
+    }
+}
