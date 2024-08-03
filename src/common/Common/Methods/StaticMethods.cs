3a4965d02693f16bc929dd8991fcd7005c72a90c
Hesron Gozano <hesron.gozano@live.com>
2024-08-03 11:02:47 +0800

    Add OTP Generator

--- src/common/Common/Methods/StaticMethods.cs
+++ src/common/Common/Methods/StaticMethods.cs
@@ -27,12 +27,17 @@ public static partial class StaticMethods
 
     public static string GeneratePassword(int length = 12, bool includeUppercase = true, bool includeDigits = true, bool includeSpecialChars = true)
     {
+        if (length == 0)
+        {
+            length = 12;
+        };
+
         string lowerCase = "abcdefghijklmnopqrstuvwxyz";
         string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         string numbers = "0123456789";
         string specialCharacters = "!@#$%^&*()";
 
-    StringBuilder passwordBuilder = new();
+        StringBuilder passwordBuilder = new();
 
         // Create a pool of characters based on the selected options
         string characters = lowerCase;
@@ -50,6 +55,25 @@ public static partial class StaticMethods
         {
             int randomIndex = random.Next(characters.Length);
             passwordBuilder.Append(characters[randomIndex]);
+        }
+
+        return passwordBuilder.ToString();
+    }
+
+    public static string GenerateOTP(int length = 6)
+    {
+        if (length == 0)
+        {
+            length = 6;
+        };
+
+        string numbers = "0123456789";
+        StringBuilder passwordBuilder = new();
+        Random random = new();
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = random.Next(numbers.Length);
+            passwordBuilder.Append(numbers[randomIndex]);
         }
 
         return passwordBuilder.ToString();
