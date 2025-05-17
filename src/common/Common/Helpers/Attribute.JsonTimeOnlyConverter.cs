21bf54e4eec50e0df76aba471c578b3da224329e
Hesron Gozano <hesron.gozano@live.com>
2025-05-17 19:21:55 +0800

    Add jsontime converter

---
+++ src/common/Common/Helpers/Attribute.JsonTimeOnlyConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace AsteriskDotHMG.Common.Helpers;
+
+public class JsonTimeOnlyConverter : STJ.JsonConverter<TimeOnly>
+{
+    public bool WithoutSeconds { get; init; }
+
+    public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var input = reader.GetString();
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return default;
+        }
+
+        return TimeOnly.Parse(input);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(
+            value.ToString(WithoutSeconds ? "HH:mm" : "HH:mm:ss")
+        );
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public class JsonTimeOnlyConverterAttribute : STJ.JsonConverterAttribute
+{
+    public bool WithoutSeconds { get; init; }
+
+    public override STJ.JsonConverter CreateConverter(Type typeToConvert)
+    {
+        return new JsonTimeOnlyConverter
+        {
+            WithoutSeconds = WithoutSeconds
+        };
+    }
+}
